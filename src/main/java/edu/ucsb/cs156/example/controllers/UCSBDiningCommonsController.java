75c3e7c9e40f7a69d1c0723e1c0354da4f104173
Austin Mac <47233575+austinmacmath@users.noreply.github.com>
2022-04-28 14:46:07 -0700

    Update UCSBDiningCommonsController.java

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsController.java
@@ -100,6 +100,7 @@ public class UCSBDiningCommonsController extends ApiController {
         UCSBDiningCommons commons = ucsbDiningCommonsRepository.findById(code)
                 .orElseThrow(() -> new EntityNotFoundException(UCSBDiningCommons.class, code));
 
+        
         commons.setName(incoming.getName());  
         commons.setHasSackMeal(incoming.getHasSackMeal());
         commons.setHasTakeOutMeal(incoming.getHasTakeOutMeal());
